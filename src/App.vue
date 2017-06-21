d95952ad0474d23f98b114d6a0e835d33f122658
HarrisChang <jone09@outlook.com>
2017-06-21 23:39:17 +0800

    header finished

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link :to="{name:'goods'}">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{name:'ratings'}">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{name:'seller'}">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      I am content!
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -23,14 +27,42 @@ export default {
 </script>
 
 <style lang="less" rel="stylesheet/less">
+  @media (min-device-pixel-ratio:1.5){
+    .border-1px:after{
+      transform: scaleY(0.7)
+    }
+  }
+  @media (min-device-pixel-ratio:2){
+    .border-1px:after{
+      transfrom:scaleY(0.5)
+    }
+  }
   #app{
     .tab{
       width:100%;
       height:40px;
       line-height:40px;
       display: flex;
+      position: relative;
+      &:after{
+        content: '';
+        display: block;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width:100%;
+        border-top: 1px solid rgba(7, 17, 27, 0.1)
+      }
       .tab-item{
         flex:1;
+        text-align: center;
+        a{
+          font-size: 14px;
+          color: rgb(77,85,93);
+          &.router-link-active{
+            color:rgb(240,20,20)
+          }
+        }
       }
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import vHeader from '@/components/header/header'
+import Goods from '@/components/goods/goods'
+import Seller from '@/components/seller/seller'
+import Ratings from '@/components/ratings/ratings'
 
 Vue.use(Router)
 
@@ -9,8 +12,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'header',
-      component: vHeader
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
     }
   ]
 })
